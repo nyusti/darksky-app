b1787970b0a0d75dcb27a10bc58840fb7c1c60f3
Richárd Nyüsti <richard.nyusti@outlook.com>
2018-11-03 13:53:24 +0100

    made some styling

--- src/DarkSky.Application/Domain/Model/Forecast.partial.cs
+++ src/DarkSky.Application/Domain/Model/Forecast.partial.cs
@@ -9,7 +9,7 @@
     {
         public string TemperatureAsString => string.Format("{0}° - {1}°", Math.Round(this.TemperatureLow ?? 0), Math.Round(this.TemperatureHigh ?? 0));
 
-        public string Feels => string.Format("{0}° - {1}°", Math.Round(this.ApparentTemperatureLow ?? 0), Math.Round(this.ApparentTemperatureHigh ?? 0));
+        public string FeelsAsString => string.Format("({0}° - {1}°)", Math.Round(this.ApparentTemperatureLow ?? 0), Math.Round(this.ApparentTemperatureHigh ?? 0));
 
         public double WindSpeedRounded => Math.Round(this.WindSpeed ?? 0);
 
