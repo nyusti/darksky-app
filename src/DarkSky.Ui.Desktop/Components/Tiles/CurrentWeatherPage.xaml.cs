571e96f552b74f530fc4f451d6c8c7f9681ab60b
Richárd Nyüsti <richard.nyusti@outlook.com>
2018-11-03 21:35:58 +0100

    Navigation refactoring (#3)
    * refactoring to use navigation
    removed unused components

---
+++ src/DarkSky.Ui.Desktop/Components/Tiles/CurrentWeatherPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using Unity.Attributes;
+
+namespace DarkSky.Ui.Desktop.Components.Tiles
+{
+    /// <summary>
+    /// Interaction logic for CurrentWeatherPage.xaml
+    /// </summary>
+    public partial class CurrentWeatherPage : Page
+    {
+        public CurrentWeatherPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the view model.
+        /// </summary>
+        /// <value>The view model.</value>
+        [Dependency]
+        public CurrentWeatherPageViewModel ViewModel
+        {
+            get => this.DataContext as CurrentWeatherPageViewModel;
+            set => this.DataContext = value;
+        }
+    }
+}
---
+++ src/DarkSky.Ui.Desktop/Components/Tiles/CurrentWeatherPageViewModel.cs
@@ -0,0 +1,75 @@
+﻿namespace DarkSky.Ui.Desktop.Components.Tiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reactive.Linq;
+    using System.Reactive.Threading.Tasks;
+    using DarkSky.Application.Domain.Model;
+    using DarkSky.Application.Domain.Services;
+    using DarkSky.Ui.Desktop.Navigation;
+    using GalaSoft.MvvmLight.Views;
+
+    public class CurrentWeatherPageViewModel : ComponentViewModel
+    {
+        private readonly IForecastService forecastService;
+        private readonly INavigationService navigationService;
+        private CurrentWeather currentForecast;
+        private List<Forecast> dailyForecast;
+        private bool isBusy;
+        private bool isReady;
+
+        public CurrentWeatherPageViewModel(IForecastService forecastService, INavigationService navigationService)
+        {
+            this.forecastService = forecastService ?? throw new ArgumentNullException(nameof(forecastService));
+            this.navigationService = navigationService ?? throw new ArgumentNullException(nameof(navigationService));
+        }
+
+        public CurrentWeather CurrentForecast
+        {
+            get => this.currentForecast;
+            set => this.Set(() => this.CurrentForecast, ref this.currentForecast, value);
+        }
+
+        public List<Forecast> DailyForecast
+        {
+            get => this.dailyForecast;
+            set => this.Set(() => this.DailyForecast, ref this.dailyForecast, value);
+        }
+
+        public bool IsBusy
+        {
+            get => this.isBusy;
+            set => this.Set(() => this.IsBusy, ref this.isBusy, value);
+        }
+
+        public bool IsReady
+        {
+            get => this.isReady;
+            set => this.Set(() => this.IsReady, ref this.isReady, value);
+        }
+
+        public override void Init()
+        {
+            if (PageNavigationService.ExtraData is Location selectedLocation)
+            {
+                this.IsReady = false;
+                this.IsBusy = true;
+                this.forecastService.GetForecastAsync(selectedLocation, ApplicationContext.UserContext.SelectedLanguage, this.CancellationToken)
+                    .ToObservable()
+                    .Finally(() =>
+                    {
+                        this.IsBusy = false;
+                        this.IsReady = true;
+                    })
+                    .Subscribe(forecast =>
+                    {
+                        this.CurrentForecast = forecast;
+                        this.DailyForecast = forecast.Daily.OrderBy(p => p.Time).ToList();
+                    }, this.CancellationToken);
+            }
+
+            base.Init();
+        }
+    }
+}
